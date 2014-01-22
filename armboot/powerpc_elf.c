9ead525989b6d723f1dd92f2396ef0c323c8211d
Maxternal <maxternal@gmail.com>
2014-01-21 20:13:06 -0600

    still getting it to compile

--- armboot/powerpc_elf.c
+++ armboot/powerpc_elf.c
@@ -633,7 +633,7 @@ int powerpc_boot_file(const char *path)
 		return 0;
 	}gecko_printf("Running Wii U code.\n");
 	for(EXIdelay = 0; EXIdelay <= 200000; EXIdelay += 10)
-	{	fres = powerpc_load_dol("/bootmii/00000003.app", &entry);
+	{	fres = powerpc_load_dol("/bootmii/00000003.app", &elfhdr.e_entry);
 		if(fres)
 		{	gecko_printf("powerpc_load_dol returned %d .\n", fres);
 			return fres;
